de7305c31845c1a495f0df5730afdd13ba05fa14
Erik <erik.97@list.ru>
2023-02-14 16:38:38 +0400

    ДЗ

---
+++ task2/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Повторите попытку");
+        }
+    }
+
+    return result;
+}
+
+int x1 = GetNumber("Введите координату  Х первой точки");
+int y1 = GetNumber("Введите координату  Y первой точки");
+int z1 = GetNumber("Введите координату  Z первой точки");
+
+int x2 = GetNumber("Введите координату  Х второй точки");
+int y2 = GetNumber("Введите координату  Y второй точки");
+int z2 = GetNumber("Введите координату  Z второй точки");
+
+double GetDistance (int x1, int x2, int y1, int y2, int z1, int z2)
+{
+    double result = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
+    return result;
+}
+
+System.Console.WriteLine(GetDistance(x1,x2,y1,y2,z1,z2));
+
+
+
---
+++ task3/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не чило или 0. Повторите попытку");
+        }
+    }
+
+    return result;
+}
+
+int x1 = GetNumber("Введите число");
+
+for (int i = 1; i <= x1; i++)
+{
+    System.Console.Write($"{Math.Pow(i,3)} ");
+}
